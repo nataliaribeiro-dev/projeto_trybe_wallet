e566843141a10bd7b85baeb4c81ecee4c7090317
Natalia Ribeiro <natilgr@hotmail.com>
2023-06-15 11:25:34 -0300

    feat(2): add header to Wallet page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <p data-testid="email-field">
+          E-mail:
+          {' '}
+          {' '}
+          { email }
+        </p>
+        <p data-testid="total-field">Despesa Total: 0 </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default Header;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
         {' '}
         TrybeWallet
+
+        <Header
+          email={ email }
+        />
       </div>
     );
   }
 }
 
-export default Wallet;
+// mapeia o estado global para a propriedade email
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+// conecta o componente com o estado global
+export default connect(mapStateToProps)(Wallet);

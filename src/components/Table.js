15f6d31e760d00f3a1a235d4d64aa2c54dc59ac6
Natalia Ribeiro <natilgr@hotmail.com>
2023-06-16 18:52:55 -0300

    add some lines and comment to use in the future

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,7 +17,25 @@ class Table extends Component {
               <th>Moeda de conversão</th>
               <th>Editar/Excluir</th>
             </tr>
+
           </thead>
+          {/* <tbody>
+            <tr>
+              <td>{descrição}</td>
+              <td>{tag}</td>
+              <td>{metodoPag}</td>
+              <td>{valor}</td>
+              <td>{moeda}</td>
+              <td>{cambio}</td>
+              <td>{valorConvertido}</td>
+              <td>{moedaDeConversao}</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          </tbody> */}
+
         </table>
       </div>
     );

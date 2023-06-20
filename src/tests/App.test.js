a81d32f67fdd8bc510611e5b375596cf3324225b
Natalia Ribeiro <natilgr@hotmail.com>
2023-06-20 17:11:14 -0300

    tests in progress

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -46,6 +46,33 @@ describe('Teste o componente <App.js />', () => {
     expect(headerCurrency).toBeInTheDocument();
   });
 
+  // test('Verifica totalValue', async () => {
+  //   const expenses = [
+  //     {
+  //       value: 0,
+  //       currency: 'USD',
+  //       exchangeRates: {},
+  //     },
+  //   ];
+
+  //   const valueTotal = expenses.reduce(
+  //     (acc, curr) => {
+  //       const { value, currency, exchangeRates } = curr;
+  //       const exchangeRate = exchangeRates[currency].ask;
+  //       const totalExpense = value * exchangeRate;
+  //       return acc + totalExpense;
+  //     },
+  //     0,
+  //   ).toFixed(2);
+
+  //   renderWithRouterAndRedux(<Header email="test@test.com" expenses={ expenses } />);
+
+  //   const totalValue = screen.getByTestId('total-field');
+  //   expect(totalValue).toBeInTheDocument();
+
+  //   await waitFor(() => expect(totalValue).toHaveTextContent(valueTotal));
+  // });
+
   test('Verifica se o componente WalletForm é renderizado corretamente', () => {
     renderWithRouterAndRedux(<WalletForm />, { initialEntries: ['/carteira'] });
 
@@ -66,5 +93,44 @@ describe('Teste o componente <App.js />', () => {
 
     const addExpenseBtn = screen.getByText('Adicionar despesa');
     expect(addExpenseBtn).toBeInTheDocument();
+  });
+
+  test('Verifica se as informações digitadas nos inputs são salvas no estado global', () => {
+    const { store } = renderWithRouterAndRedux(<WalletForm />, { initialEntries: ['/carteira'] });
+
+    const value = screen.getByLabelText('Valor');
+    expect(value).toBeInTheDocument();
+
+    const description = screen.getByLabelText('Descrição');
+    expect(description).toBeInTheDocument();
+
+    const currency = screen.getByLabelText('Moeda');
+    expect(currency).toBeInTheDocument();
+
+    const method = screen.getByLabelText('Método de pagamento');
+    expect(method).toBeInTheDocument();
+
+    const tag = screen.getByLabelText('Categoria');
+    expect(tag).toBeInTheDocument();
+
+    const addExpenseBtn = screen.getByText('Adicionar despesa');
+    expect(addExpenseBtn).toBeInTheDocument();
+
+    act(() => {
+      userEvent.type(value, '100');
+      userEvent.type(description, 'test');
+      // userEvent.selectOptions(currency, 'USD');
+      userEvent.selectOptions(method, 'Dinheiro');
+      userEvent.selectOptions(tag, 'Alimentação');
+      userEvent.click(addExpenseBtn);
+    });
+
+    const { expenses } = store.getState().wallet;
+    expect(expenses.length).toBe(1);
+    expect(expenses[0].value).toBe('100');
+    expect(expenses[0].description).toBe('test');
+    // expect(expenses[0].currency).toBe('USD');
+    expect(expenses[0].method).toBe('Dinheiro');
+    expect(expenses[0].tag).toBe('Alimentação');
   });
 });
